665d44bf9a5d7bca0ed2f5033c599be41fd8ab0f
test_glb <test_glb@t.com>
2019-04-09 14:45:40 +0800

    6

--- src/main/java/com/hand/git/Happy.java
+++ src/main/java/com/hand/git/Happy.java
@@ -3,11 +3,7 @@ package com.hand.git;
 public class Happy {
 	
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("branch hot_zw");
-=======
 		System.out.println("Test branch");
->>>>>>> branch 'master' of https://github.com/22275zw/TestGit
 	}
 
 }
