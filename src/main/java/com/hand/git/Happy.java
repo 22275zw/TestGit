2287c20e8976b42780abd12f8a19d376fc91f6c2
test_glb <test_glb@t.com>
2019-04-09 14:43:11 +0800

    m

--- src/main/java/com/hand/git/Happy.java
+++ src/main/java/com/hand/git/Happy.java
@@ -3,11 +3,7 @@ package com.hand.git;
 public class Happy {
 	
 	public static void main(String[] args) {
-		System.out.println("111666");
-		System.out.println("update zw 666");
-
-		System.out.println("update by wangwei ");
-
+		System.out.println("branch hot_zw");
 	}
 
 }
