99c71c4c68f4160b9da5153e0d3262dfd9d3493b
test_glb <test_glb@t.com>
2019-04-10 10:17:55 +0800

    commit in master

--- src/main/java/com/hand/git/Happy.java
+++ src/main/java/com/hand/git/Happy.java
@@ -4,10 +4,6 @@ public class Happy {
 
 	public static void main(String[] args) {
 		System.out.println("master");
-		System.out.println("hot_a");
-		System.out.println("hot_b");
-		System.out.println("456");
-		System.out.println("123");
 	}
 
 }
