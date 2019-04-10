b8d17d1f7278cae830ace9dc0278eca906b3786f
test_glb <test_glb@t.com>
2019-04-10 10:07:40 +0800

    commit in hot_b

--- src/main/java/com/hand/git/Happy.java
+++ src/main/java/com/hand/git/Happy.java
@@ -3,7 +3,8 @@ package com.hand.git;
 public class Happy {
 	
 	public static void main(String[] args) {
-		System.out.println("我是master");
+		System.out.println("我不是master");
+		System.out.println("我是hot_b");
 	}
 
 }
