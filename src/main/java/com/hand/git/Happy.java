60141f61ac9a7a39841a382f264fb42e5f702d65
test_glb <test_glb@t.com>
2019-07-02 16:41:48 +0800

    1

--- src/main/java/com/hand/git/Happy.java
+++ src/main/java/com/hand/git/Happy.java
@@ -4,8 +4,7 @@ public class Happy {
 
 	public static void main(String[] args) {
 		System.out.println("master");
-		System.out.println("master2");
-		System.out.println("master1");
+		System.out.println("版本1");
 
 	}
 
